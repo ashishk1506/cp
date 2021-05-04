1494d6992375e6924659637187303d869821c0f5
ashishk1506 <mahtobuddheshwar@gmail.com>
2021-05-04 23:10:30 +0530

    done

--- test.cpp
+++ test.cpp
@@ -2,22 +2,31 @@
 
 using namespace std;
 
-class AlgoExpert{
-	
-	public:
-		int nextMove(int row_star, int col_start, int row_end, int col_end){
-			
-			
-
-
-
-
-			return arr;
-		}
-
-}
-
 int main(){
-	
+
+int isFour(int);
+int n;
+cin>>n;
+cout<<isFour(n);
 
 }
+
+      int isFour(int n){
+            int count = 0;
+            int sum = 0;
+            for(int i=2; i*i<=n; i++){
+                if(n%i==0)
+                {
+                    sum = sum + i + n/i;
+                    count++;
+                }
+                if(count>1)
+                {
+                    return 0;
+                }
+            }
+            if(count == 1)
+                return sum + n + 1;
+            else
+                return 0;
+        }
